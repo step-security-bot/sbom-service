cfc0cd12594b4ace6b15c6e3ec064fb605942dd0
zejun19 <wangzejun19@outlook.com>
2022-08-23 16:10:51 +0800

    test: repodata test init data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -134,6 +134,6 @@ ON CONFLICT (id) DO UPDATE
     SET name = EXCLUDED.name, attribute = EXCLUDED.attribute;
 
 INSERT INTO product(id, name, attribute)
-VALUES('11111111-1111-41e7-b97a-a3481bb6e666', 'repodataTest', '{"arg":"6"}'::jsonb)
+VALUES('11111111-1111-41e7-b97a-a3481bb6e777', 'repodataTest', '{"arg":"7"}'::jsonb)
 ON CONFLICT (id) DO UPDATE
     SET name = EXCLUDED.name, attribute = EXCLUDED.attribute;
