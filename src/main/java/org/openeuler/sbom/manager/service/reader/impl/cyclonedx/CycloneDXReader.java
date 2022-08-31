5b04a8436ef7f8d5e55fc4ca7fbc0e1dc30224eb
zejun19 <wangzejun19@outlook.com>
2022-08-31 16:04:44 +0800

    feat: split sbom reader to readToDocument & persistSbom

--- src/main/java/org/openeuler/sbom/manager/service/reader/impl/cyclonedx/CycloneDXReader.java
+++ src/main/java/org/openeuler/sbom/manager/service/reader/impl/cyclonedx/CycloneDXReader.java
@@ -1,5 +1,7 @@
 package org.openeuler.sbom.manager.service.reader.impl.cyclonedx;
 
+import org.openeuler.sbom.manager.model.Sbom;
+import org.openeuler.sbom.manager.model.sbom.SbomDocument;
 import org.opensourceway.sbom.constants.SbomConstants;
 import org.openeuler.sbom.manager.service.reader.SbomReader;
 import org.openeuler.sbom.manager.utils.SbomFormat;
@@ -21,4 +23,15 @@ public class CycloneDXReader implements SbomReader {
     public void read(String productName, SbomFormat format, byte[] fileContent) throws IOException {
 
     }
+
+    @Override
+    public SbomDocument readToDocument(String productName, SbomFormat format, byte[] fileContent) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Sbom persistSbom(String productName, SbomDocument sbomDocument) {
+        return null;
+    }
+
 }
