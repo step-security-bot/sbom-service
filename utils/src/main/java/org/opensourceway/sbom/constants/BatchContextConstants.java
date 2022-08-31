f8b436a741ee5316694cab07f60c4115a310eecf
zejun19 <wangzejun19@outlook.com>
2022-08-31 16:10:38 +0800

    feat: enable spring batch

---
+++ utils/src/main/java/org/opensourceway/sbom/constants/BatchContextConstants.java
@@ -0,0 +1,21 @@
+package org.opensourceway.sbom.constants;
+
+public class BatchContextConstants {
+
+    public final static String BATCH_RAW_SBOM_ID_KEY = "BATCH_RAW_SBOM_ID";
+
+    public final static String BATCH_SBOM_ID_KEY = "BATCH_SBOM_ID";
+
+    public final static String BATCH_SBOM_PRODUCT_NAME_KEY = "BATCH_SBOM_PRODUCT_NAME";
+
+    public final static String BATCH_SBOM_SPEC_KEY = "BATCH_SBOM_SPEC";
+
+    public final static String BATCH_SBOM_FORMAT_KEY = "BATCH_SBOM_FORMAT";
+
+    public final static String BATCH_RAW_SBOM_BYTES_KEY = "BATCH_RAW_SBOM_BYTES";
+
+    public final static String BATCH_SBOM_DOCUMENT_KEY = "BATCH_SBOM_DOCUMENT";
+
+    public final static String BATCH_PRODUCT_TYPE_KEY = "productType";
+
+}
