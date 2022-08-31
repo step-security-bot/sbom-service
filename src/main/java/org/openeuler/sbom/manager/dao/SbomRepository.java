ec0102b96f9909f5f29f8ec9680cd13ddedfad3b
zejun19 <wangzejun19@outlook.com>
2022-08-31 15:26:27 +0800

    fix: entity ID type error

--- src/main/java/org/openeuler/sbom/manager/dao/SbomRepository.java
+++ src/main/java/org/openeuler/sbom/manager/dao/SbomRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface SbomRepository extends JpaRepository<Sbom, String> {
+public interface SbomRepository extends JpaRepository<Sbom, UUID> {
 
     @Query(value = "SELECT * FROM sbom WHERE product_id = (SELECT id FROM product WHERE name = :productName)",
             nativeQuery = true)
