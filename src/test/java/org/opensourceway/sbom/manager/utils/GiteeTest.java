db2c2ad9fa20a5d5df7ddcc22f59324ba14b0c35
zejun19 <wangzejun19@outlook.com>
2022-09-28 16:54:28 +0800

    test: gitlab api support search file and get file content

--- src/test/java/org/opensourceway/sbom/manager/utils/GiteeTest.java
+++ src/test/java/org/opensourceway/sbom/manager/utils/GiteeTest.java
@@ -3,15 +3,21 @@ package org.opensourceway.sbom.manager.utils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.opensourceway.sbom.clients.vcs.VcsApi;
+import org.opensourceway.sbom.clients.vcs.gitee.model.GiteeFileInfo;
+import org.opensourceway.sbom.constants.SbomConstants;
 import org.opensourceway.sbom.manager.TestConstants;
+import org.opensourceway.sbom.openeuler.obs.SbomRepoConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class GiteeTest {
@@ -44,6 +50,50 @@ public class GiteeTest {
         if (tmpDir.exists()) {
             Assertions.fail("failed to delete test repository archive temDir: %s".formatted(tmpDirPath));
         }
+    }
+
+    @Test
+    public void getSingleFileContextTest() {
+        List<GiteeFileInfo> fileInfos = giteeApi.findRepoFiles(ARCHIVE_DOWNLOAD_ORG,
+                ARCHIVE_DOWNLOAD_REPO,
+                ARCHIVE_DOWNLOAD_BRANCH,
+                SbomConstants.LINUX_FILE_SYSTEM_SEPARATOR,
+                SbomRepoConstants.SPEC_FILE_NAME_REGEX);
+
+        Assertions.assertThat(fileInfos.size()).isEqualTo(1);
+
+        Optional<GiteeFileInfo> fileInfoOptional = fileInfos.stream().findFirst();
+        Assertions.assertThat(fileInfoOptional.isPresent()).isTrue();
+        Assertions.assertThat(fileInfoOptional.get().name()).isEqualTo("389-ds-base.spec");
+    }
+
+    @Test
+    public void getMultiFileContextTest() {
+        List<GiteeFileInfo> fileInfos = giteeApi.findRepoFiles(ARCHIVE_DOWNLOAD_ORG,
+                ARCHIVE_DOWNLOAD_REPO,
+                ARCHIVE_DOWNLOAD_BRANCH,
+                SbomConstants.LINUX_FILE_SYSTEM_SEPARATOR,
+                SbomRepoConstants.YAML_FILE_NAME_REGEX);
+        Assertions.assertThat(fileInfos.size()).isEqualTo(2);
+        Assertions.assertThat(fileInfos.get(0).name()).isEqualTo("389-ds-base.yaml");
+        Assertions.assertThat(fileInfos.get(1).name()).isEqualTo("jemalloc.yaml");
+    }
+
+    @Test
+    public void notFoundFileContextTest() {
+        Exception exception = null;
+        try {
+            giteeApi.findRepoFiles(ARCHIVE_DOWNLOAD_ORG,
+                    "openEuler-kernel",
+                    ARCHIVE_DOWNLOAD_BRANCH,
+                    SbomConstants.LINUX_FILE_SYSTEM_SEPARATOR,
+                    SbomRepoConstants.SPEC_FILE_NAME_REGEX);
+        } catch (Exception e) {
+            exception = e;
+        }
+
+        Assertions.assertThat(exception).isNotNull();
+        Assertions.assertThat(exception instanceof WebClientResponseException.NotFound).isNotNull();
     }
 
 }
