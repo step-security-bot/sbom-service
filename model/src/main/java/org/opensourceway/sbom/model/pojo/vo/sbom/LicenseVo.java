ec2cda4a07fca0cb4f00f02cba73bc3fde8d3cc8
huanceng <hhq0802@foxmail.com>
2023-02-23 14:29:37 +0800

    Chore: list packages with detailed license info

--- model/src/main/java/org/opensourceway/sbom/model/pojo/vo/sbom/LicenseVo.java
+++ model/src/main/java/org/opensourceway/sbom/model/pojo/vo/sbom/LicenseVo.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.opensourceway.sbom.model.entity.License;
 
 import java.io.Serializable;
-import java.math.BigInteger;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LicenseVo implements Serializable {
@@ -16,8 +15,6 @@ public class LicenseVo implements Serializable {
     private boolean isLegal;
 
     private String licenseUrl;
-
-    private BigInteger count;
 
     public String getLicenseId() {
         return licenseId;
@@ -49,14 +46,6 @@ public class LicenseVo implements Serializable {
 
     public void setLicenseUrl(String licenseUrl) {
         this.licenseUrl = licenseUrl;
-    }
-
-    public BigInteger getCount() {
-        return count;
-    }
-
-    public void setCount(BigInteger count) {
-        this.count = count;
     }
 
     public static LicenseVo fromLicense(License license) {
