a73bd9a4f66bbc33385a6392ddc96f2bd8339f3b
huanceng <hhq0802@foxmail.com>
2023-03-03 12:08:42 +0800

    Fix: rollback `count` in `LicenseVo`

--- model/src/main/java/org/opensourceway/sbom/model/pojo/vo/sbom/LicenseVo.java
+++ model/src/main/java/org/opensourceway/sbom/model/pojo/vo/sbom/LicenseVo.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.opensourceway.sbom.model.entity.License;
 
 import java.io.Serializable;
+import java.math.BigInteger;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LicenseVo implements Serializable {
@@ -15,6 +16,8 @@ public class LicenseVo implements Serializable {
     private boolean isLegal;
 
     private String licenseUrl;
+
+    private BigInteger count;
 
     public String getLicenseId() {
         return licenseId;
@@ -46,6 +49,14 @@ public class LicenseVo implements Serializable {
 
     public void setLicenseUrl(String licenseUrl) {
         this.licenseUrl = licenseUrl;
+    }
+
+    public BigInteger getCount() {
+        return count;
+    }
+
+    public void setCount(BigInteger count) {
+        this.count = count;
     }
 
     public static LicenseVo fromLicense(License license) {
